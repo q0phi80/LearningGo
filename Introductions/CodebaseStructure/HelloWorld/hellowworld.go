1dca27915002a5e4a8fe0beb86aa5f8766289ec3
q0phi80 <lab.pwc@gmail.com>
2020-04-23 13:15:25 -0400

    Started custom package creation.

---
+++ Introductions/CodebaseStructure/HelloWorld/hellowworld.go
@@ -0,0 +1,7 @@
+package helloworld
+
+// The function is capitalized because it is going to be a public function.
+func GetHellowWorld() string { // Going to return a string.
+    return "Hello, World!" // The string the function is going to return.
+
+}
---
+++ Introductions/CodebaseStructure/HelloWorld/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "src/LearningGo/Introductions/CodebaseStructure/HelloWorld"
+)
+
+func main() {
+    fmt.Println(helloworld.GetHellowWorld())
+}
