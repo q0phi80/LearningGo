e899eb3d57ac693dd0d52cc2101c85f6a437370a
q0phi80 <lab.pwc@gmail.com>
2020-04-24 08:47:50 -0400

    Added few additional codes.

---
+++ Introductions/Viper/main4.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "fmt"
+  "flag"
+  "log"
+  "github.com/spf13/viper"
+)
+var username, password string
+
+func useCredentials() {
+    fmt.Printf("Connecting to %s at %s\n", viper.GetString("webserver.name"), viper.GetString("webserver.endpoint"))
+    fmt.Printf("Your username is %s\n", viper.GetString("credentials.username"))
+    fmt.Printf("Your password is %s\n", viper.GetString("credentials.password"))
+}
+
+func main() {
+    flag.StringVar(&username, "user", "", "Username to use.")
+    flag.StringVar(&password, "pass", "", "Password to use.")
+    flag.Parse()
+    viper.SetDefault("webserver.endpoint", "https://httpbin.org/get") // Setting default means the value can be overwritten.
+    viper.SetDefault("webserver.name", "Testing Endpint")
+    viper.AddConfigPath(".")
+    viper.SetConfigName("creds")
+    viper.ReadInConfig()
+    /*if err := viper.ReadInConfig(); err != nil {
+        log.Fatalln("[!] Unable to read config.")
+    }*/
+    if username != "" {
+        viper.Set("credentials.username", username)
+    }
+    if password != "" {
+        viper.Set("credentials.password", password)
+    }
+    if !viper.IsSet("credentials.username") || !viper.IsSet("credentials.password") {
+        log.Fatalln("[!] No credentials supplied.")
+    }
+    useCredentials()
+}
